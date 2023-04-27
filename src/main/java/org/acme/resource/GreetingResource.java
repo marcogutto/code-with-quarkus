87bd73b4e650654e11ff61898e9a581038b51805
Marco Seabra <marcogutto@brq.com>
2023-04-27 11:11:15 -0300

    Ajustes ...

--- src/main/java/org/acme/resource/GreetingResource.java
+++ src/main/java/org/acme/resource/GreetingResource.java
@@ -5,12 +5,39 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import io.smallrye.mutiny.Uni;
+
 @Path("/hello")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         return "Hello from RESTEasy Reactive";
+    }
+
+    @GET
+    @Path(value = "/mutiny")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Uni<String> helloMutiny() {
+        return Uni.createFrom().item("Hello from RESTEasy Reactive").onItem().transform(h -> {
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            return h;
+        }).onFailure().recoverWithNull();
     }
 }
