1045f2325fee3f04bed8567d3844ce3901003c84
Marco Seabra <marcogutto@brq.com>
2023-04-28 19:59:02 -0300

    Ajuste SucoParcial ...

--- src/main/java/org/acme/domain/entity/SucoParcial.java
+++ src/main/java/org/acme/domain/entity/SucoParcial.java
@@ -1,10 +1,7 @@
 package org.acme.domain.entity;
 
-import java.io.Serializable;
 import java.math.BigDecimal;
 
-import io.quarkus.mongodb.panache.common.MongoEntity;
-import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -15,8 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 
-@MongoEntity(collection = "suco-parcial")
-public class SucoParcial extends ReactivePanacheMongoEntity implements Serializable{
+public class SucoParcial {
 
     private String descricao;
 
