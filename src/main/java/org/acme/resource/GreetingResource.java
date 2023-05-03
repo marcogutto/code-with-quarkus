c81d1e4f2e12c17ae313d4fc85ff0c6c06cb38c1
Marco Seabra <marcogutto@brq.com>
2023-05-03 10:00:30 -0300

    Ajuste mutiny ...

--- src/main/java/org/acme/resource/GreetingResource.java
+++ src/main/java/org/acme/resource/GreetingResource.java
@@ -5,33 +5,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import io.smallrye.mutiny.Uni;
-
 @Path("/hello")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public Uni<String> hello() {
-        System.out.println("Inicio ...");
-
-        Uni<String> resposta = Uni.createFrom().item("Hello from RESTEasy Reactive").onItem().transform(t -> {
-
-            System.out.println("Processando ...");
-
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-
-            return t;
-
-        });
-
-        System.out.println("Entregue ...");
-
-        return resposta;
+    public String hello() {
+        return "Hello from RESTEasy Reactive";
     }
 }
